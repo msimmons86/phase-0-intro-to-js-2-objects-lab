00b9821aad8262d8f63ad37db8663fde4a269557
Michael Simmons <msimmons86@gmail.com>
2022-03-31 18:50:16 -0700

    commit tree 2c5e7e81540e4cfcf13b889114da3eaeef3b48d3

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+let employee = {
+  streetAddress: "72 Charles Street",
+  name: "Harley Simmons",
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return {
+    ...employee,
+    ["name"]: "Sam",
+    ["streetAddress"]: "11 Broadway",
+  }
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee.streetAddress = "12 Broadway"
+  return employee
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newObject = Object.assign ({}, employee)
+    delete newObject[key]
+    return newObject
+  }
+
+
+function  destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key]
+  return employee
+}
